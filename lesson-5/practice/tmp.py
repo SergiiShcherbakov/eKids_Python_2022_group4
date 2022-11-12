b89801dc610227a4d1f77309ef82888b8ee78ac1
Anton_Gontar <anton_gontar@epam.com>
2022-11-12 13:08:24 +0200

    add materials for sixth lesson

---
+++ lesson-5/practice/tmp.py
@@ -0,0 +1,15 @@
+items = []
+
+print('1. items ' + str(items))
+
+items.append("a")
+items.append("b")
+items.append("C")
+
+print('2. items ' + str(items))
+
+
+print('=============')
+items = ['a', 'b', 'c', 'd']
+for qwerty in items:
+    print('item: ' + qwerty)
